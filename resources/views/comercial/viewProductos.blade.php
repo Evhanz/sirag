b3ab0715437da0ca963a013b7658155734fa1acf
Eidelman Hernandez <eidel_hs@hotmail.com>
2016-08-19 18:09:36 -0500

    coreccion v2_1_1
    correccion de css , se elimino a botonera auxiliar

--- resources/views/comercial/viewProductos.blade.php
+++ resources/views/comercial/viewProductos.blade.php
@@ -91,15 +91,7 @@
                     <!-- Box (with bar chart) -->
                     <div class="box box-info" id="box_maestro">
                         <div class="box-header">
-                            <!-- tools box -->
-                            <div class="pull-right box-tools">
-                                <button class="btn btn-danger btn-sm refresh-btn" data-toggle="tooltip" title="Reload"><i class="fa fa-refresh"></i></button>
-                                <button class="btn btn-danger btn-sm" data-widget='collapse' data-toggle="tooltip" title="Collapse"><i class="fa fa-minus"></i></button>
-                                <button class="btn btn-danger btn-sm" data-widget='remove' data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button>
-                            </div><!-- /. tools -->
-                            <i class="fa fa-cloud"></i>
 
-                            <h3 class="box-title">Data Procesada </h3>
                         </div><!-- /.box-header -->
                         <div class="box-body ">
 
