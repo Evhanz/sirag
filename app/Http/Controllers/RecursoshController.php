981beac19a1ebec7e3c8f1537f1691604104f090
Evhanz <eidel_hs@hotmail.com>
2016-10-31 12:54:56 -0500

    s

--- app/Http/Controllers/RecursoshController.php
+++ app/Http/Controllers/RecursoshController.php
@@ -336,7 +336,7 @@ class RecursoshController extends Controller
 
         $suma = 0;
 
-        /*
+       
 
         foreach ($data as $i){
 
@@ -345,9 +345,9 @@ class RecursoshController extends Controller
 
         //sumamos primero las cuentas de abono
         $suma = $suma + floatval(substr($c_cargo, 3, 7));
-        */
+        
         //return $suma;
-        return 'enro aqui';
+        return $suma;
     }
 
 
