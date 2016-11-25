712a2a982ee94ca229da9302f0865397e26dc24d
Evhanz <eidel_hs@hotmail.com>
2016-11-25 17:57:37 +0000

    rev

--- app/Http/Controllers/RecursoshController.php
+++ app/Http/Controllers/RecursoshController.php
@@ -243,7 +243,7 @@ class RecursoshController extends Controller
             //4.- e coloca el valor fijo tipo de DNI(1,3,5) seguido del DNI
             $row = $row.$i->TIPO_DOCUMENTO.$i->DNI.$this->getEspacioBlanco(strlen($i->DNI),15);
             //5.- se coloca el nombre 75 max
-            $row = $row.utf8_decode($i->Nombre).$this->getEspacioBlanco(strlen(utf8_encode($i->Nombre)),75);
+            $row = $row.utf8_decode($i->Nombre).$this->getEspacioBlanco(strlen($i->Nombre),75);
             //6.- referencia Beneficiario
             $row = $row.'Referencia Beneficiario '.$i->DNI.$this->getEspacioBlanco(strlen($i->DNI),16);
             //7.- referencia del empleado
