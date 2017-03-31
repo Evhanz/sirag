e933ae249fa483ac378e264bc0b811ff6740e413
Eidelman Hernandez <eidel_hs@hotmail.com>
2017-03-31 18:25:28 -0500

    rev

--- app/Http/Controllers/Packing/MateriaPrimaController.php
+++ app/Http/Controllers/Packing/MateriaPrimaController.php
@@ -11,16 +11,19 @@ namespace App\Http\Controllers\Packing;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use sirag\Repositories\packing\MateriaPrimaRep;
 use sirag\Repositories\PersonalRep;
 
 class MateriaPrimaController extends Controller
 {
 
     protected  $personalRep;
+    protected  $materiaPrimaRep;
 
-    public function __construct(PersonalRep $personalRep)
+    public function __construct(PersonalRep $personalRep,MateriaPrimaRep $materiaPrimaRep)
     {
         $this->personalRep = $personalRep;
+        $this->materiaPrimaRep = $materiaPrimaRep;
 
     }
 
@@ -50,6 +53,15 @@ class MateriaPrimaController extends Controller
     public function materiaPrimaStoreNew()
     {
         $data = \Input::all();
+
+        $fecha = $data['cabecera']['fecha'];
+        $cabecera = \DB::select("SELECT * FROM sirag.ingreso_MP  where fecha = '$fecha'");
+
+        if(count($cabecera)>=1){
+            $data = ['respuesta'=>'ya existe un valor con la misma fecha'];
+        }else{
+            $this->materiaPrimaRep->store($data);
+        }
 
 
         return \Response::json($data);
--- app/Logica/Repositories/packing/MateriaPrimaRep.php
+++ app/Logica/Repositories/packing/MateriaPrimaRep.php
@@ -27,80 +27,93 @@ class MateriaPrimaRep
     public function store($data)
     {
 
-        //primero guardamos la cabecera
 
-        $cabecera = $data['cabecera'];
+        \DB::transaction(function () use ($data) {
 
-        $guia = $cabecera['guia_transportista'];
-        $h_inicio = $cabecera['h_inicio'];
-        $h_fin = $cabecera['h_fin'];
-        $dni_responsable = $cabecera['responsable'];
-        $dni_controlador = $cabecera['controlador'];
-        $id_productor = $cabecera['id_productor'];
-        $fecha = $cabecera['fecha'];
+            //primero guardamos la cabecera
 
-        \DB::insert("INSERT INTO sirag.ingreso_MP (guia_transportista,h_inicio,h_fin
-                    ,dni_responsable,dni_controlador,id_productor) VALUES
+            $cabecera = $data['cabecera'];
+
+            $guia = $cabecera['guia_transportista'];
+            $h_inicio = $cabecera['h_inicio'];
+            $h_fin = $cabecera['h_fin'];
+            $dni_responsable = $cabecera['responsable'];
+            $dni_controlador = $cabecera['controlador'];
+            $id_productor = '1';
+            $fecha = $cabecera['fecha'];
+
+            \DB::insert("INSERT INTO sirag.ingreso_MP (guia_transportista,h_inicio,h_fin
+                    ,dni_responsable,dni_controlador,id_productor,fecha) VALUES
                      ('$guia', '$h_inicio', '$h_fin','$dni_responsable','$dni_controlador',1,'$fecha')");
 
-        $cabecera = \DB::select("SELECT * FROM sirag.ingreso_MP  where fecha = '$fecha'");
-        $id_cabecera = $cabecera->id;
+            $cabecera = \DB::select("SELECT * FROM sirag.ingreso_MP  where fecha = '$fecha'");
+            $id_cabecera = $cabecera[0]->id;
 
 
-        //detalle uva
+            //detalle uva
 
-        $detalle_uva = $data['detalle_uva'];
+            $detalle_uva = $data['detalle_uva'];
 
-        foreach ($detalle_uva as $item){
+            foreach ($detalle_uva as $item){
 
-            $n_pesadas = $item['n_pesadas'];
-            $n_guia = $item['guia'];
-            $variedad = $item['variedad'];
-            $fundo_parron = $item['fundo'].'/'.$item['parron'];
-            $l_produccion = $item['l_produccion'];
-            $n_jaba = $item['n_jaba'];
-            $tara_jaba = $item['tara_jaba'];
-            $tara_parihuela = $item['tara_parihuela'];
-            $peso_bruto = $item['peso_bruto'];
+                $n_pesadas = $item['n_pesadas'];
+                $n_guia = $item['guia'];
+                $variedad = $item['variedad'];
+                $fundo_parron = $item['fundo'].'/'.$item['parron'];
+                $l_produccion = $item['l_produccion'];
+                $n_jaba = $item['n_jaba'];
+                $tara_jaba = $item['tara_jaba'];
+                $tara_parihuela = $item['tara_parihuela'];
+                $peso_bruto = $item['peso_bruto'];
 
-            \DB::insert("INSERT INTO sirag.detalle_uva (n_pesadas , n_guia , variedad ,fundo_parron
+                \DB::insert("INSERT INTO sirag.detalle_uva (n_pesadas , n_guia , variedad ,fundo_parron
                         ,l_produccion , n_jaba , tara_jaba , tara_parihuela , peso_bruto , id_ingreso_MP ) VALUES
                      ('$n_pesadas', '$n_guia', '$variedad','$fundo_parron','$l_produccion',$n_jaba,$tara_jaba,
                      $tara_parihuela,$peso_bruto,'$id_cabecera')");
 
-        }
+            }
 
-        //detalle descarte
+            //detalle descarte
 
-        $detalle_descarte = $data['detalle_descarte'];
+            $detalle_descarte = $data['detalle_descarte'];
 
-        foreach ($detalle_descarte as $item){
-
-
-            $fundo_parron = $item['fundo_parron'];
-            $variedad = $item['variedad'];
-            $racimo = $item['racimo'];
-            $baya = $item['baya'];
-            $k_racimo = $item['kl_racimo'];
-            $k_baya = $item['kl_baya'];
+            foreach ($detalle_descarte as $item){
 
 
-
-            \DB::insert("INSERT INTO sirag.detalle_uva (n_pesadas , n_guia , variedad ,fundo_parron
-                        ,l_produccion , n_jaba , tara_jaba , tara_parihuela , peso_bruto , id_ingreso_MP ) VALUES
-                     ('$n_pesadas', '$n_guia', '$variedad','$fundo_parron','$l_produccion',$n_jaba,$tara_jaba,
-                     $tara_parihuela,$peso_bruto,'$id_cabecera')");
-
-        }
-
-        //detalle de choferes
-        $conductores = $data['conductores'];
-
-        foreach ($conductores as $item){
+                $fundo_parron = $item['fundo_parron'];
+                $variedad = $item['variedad'];
+                $racimo = $item['racimo'];
+                $baya = $item['baya'];
+                $k_racimo = $item['kl_racimo'];
+                $k_baya = $item['kl_baya'];
+                $porcentaje = $item['porcentaje'];
+                $total = $item['total'];
 
 
+                \DB::insert("INSERT INTO sirag.detalle_descarte (fundo_parron , variedad , racimo ,baya
+                        ,k_racimo , k_baya , porcentaje , total , id_ingreso_MP ) VALUES
+                     ('$fundo_parron', '$variedad', $racimo,$baya,$k_racimo,$k_baya,$porcentaje,
+                     $total,'$id_cabecera')");
 
-        }
+            }
+
+            //detalle de choferes
+            $conductores = $data['conductores'];
+
+            foreach ($conductores as $item){
+
+                $dni = $item['dni'];
+                $placa = $item['placa'];
+
+                \DB::insert("INSERT INTO sirag.detalle_chofer_mp (placa , dni_chofer , id_ingreso_mp ) VALUES
+                     ('$placa', '$dni', $id_cabecera)");
+            }
+
+
+            return 'correcto';
+        });
+
+
 
 
 
