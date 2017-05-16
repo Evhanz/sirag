b26240d6ee27e6b7cc96c04d741f25ae52525538
Eidelman Hernandez <eidel_hs@hotmail.com>
2017-05-16 10:45:55 -0500

    rev

--- resources/views/packing/pallet/viewNewPallet.blade.php
+++ resources/views/packing/pallet/viewNewPallet.blade.php
@@ -3,10 +3,10 @@
 @section('header')
     <h1 xmlns:v-on="http://www.w3.org/1999/xhtml" xmlns:v-on="http://www.w3.org/1999/xhtml">
         Dashboard
-        <small>Módulo Pallet</small>
+        <small>Módulo Materia Prima</small>
     </h1>
     <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Modulo: Pallet</a></li>
+        <li><a href="#"><i class="fa fa-dashboard"></i> Modulo: Materia Prima</a></li>
         <li class="active">New</li>
     </ol>
 
@@ -41,100 +41,73 @@
             <div class="box-body">
 
                 <div class="row" >
-                    <div class="col-xs-5 col-xs-offset-1" >
+                    <div class="col-xs-4 col-xs-offset-1" >
 
                         <h1>PALLET</h1>
 
                     </div>
 
-                    <div class="col-xs-5">
-                        <h2><strong>Código:</strong> @{{ codigo }}</h2>
-                    </div>
-
-
                 </div>
-                <div class="row">
-                    <div class="col-xs-offset-1 col-xs-10">
-
-                    </div>
-                </div>
+                <br>
 
                 <div class="row">
 
-                    <div class="col-xs-5 col-xs-offset-1">
-
-                        <div class="row">
-                            <div class="col-xs-12">
-                                DESCRIPCION <BR>
-                                <input type="text" class="form-control" v-model="pallet.descripcion">
-                            </div>
-                        </div>
-                        <br>
-
-                        <div class="row">
-                            <div class="col-xs-12 " >
-                                <a id="btnEnviar" @click="saveData()" class="btn btn-success btn-lg"> Guardar  <i class="fa fa-save"></i></a>
-                            </div>
-                        </div>
+                    <div class="col-xs-4 col-xs-offset-1">
+                        DESCRIPCION <BR>
+                        <input type="text" class="form-control" v-model="pallet.descripcion">
 
                     </div>
-
-                    <!--
                     <div class="col-xs-4">
                         Fecha de Vencimiento
                         <input type="date" class="form-control" v-model="pallet.fecha_vencimiento">
 
                     </div>
-                    -->
+                </div>
+                <br><br>
 
-                    <div class="col-xs-5">
-                        <div class="row" style="position: relative;">
-                            <div class="col-xs-2" style="text-align: right;position: fixed;top: 250px;right: 90px;z-index: 1;">
-                                <a   @click="addDetail()" style="cursor: pointer">
-                                    <i class="fa fa-plus-circle fa-3x" aria-hidden="true"></i>
-                                </a>
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-xs-12 " >
-                                <table class="table">
-                                    <thead>
-                                    <tr>
-                                        <th>*</th>
-                                        <th>Código</th>
-                                        <th>Descripcion</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    <tr v-for="(item, index) in detalles">
-                                        <td>
-                                            <a class="btn btn-danger btn-xs" @click="quitDetail(index)"> <i class="fa fa-minus-circle"></i> </a>
-                                        </td>
-                                        <td>
-                                            <input  v-focus :id="index" @keyup.enter="getCaja(item.id_caja,item,index)" v-model="item.id_caja" type="text" class="form-control">
-                                        </td>
-                                        <td>
-                                            -
-                                        </td>
-
-                                    </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-
+                <div class="row">
+                    <div class="col-xs-8 col-xs-offset-1" style="text-align: center">
+                        <a   @click="addDetail()" style="cursor: pointer">
+                            <i class="fa fa-plus-circle fa-5x" aria-hidden="true"></i>
+                        </a>
                     </div>
+                </div>
 
+                <br>
+
+                <div class="row">
+                    <div class="col-xs-8 col-xs-offset-1" >
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th>*</th>
+                                <th>Código</th>
+                                <th>Descripcion</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr v-for="(item, index) in detalles">
+                                <td>
+                                    <a class="btn btn-danger btn-xs" @click="quitDetail(index)"> <i class="fa fa-minus-circle"></i> </a>
+                                </td>
+                                <td>
+                                    <input :id="index" @keyup.enter="getCaja(item.id_caja,item,index)" v-model="item.id_caja" type="text" class="form-control">
+                                </td>
+                                <td>
+                                    -
+                                </td>
+
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
 
                 <div class="row">
-                    <div class="col-xs-offset-1 col-xs-10">
-                        <hr>
+                    <div class="col-xs-8 col-xs-offset-1" style="text-align: right">
+                        <a id="btnEnviar" @click="saveData()" class="btn btn-success btn-lg"> Guardar  <i class="fa fa-save"></i></a>
                     </div>
                 </div>
-
-
 
 
 
