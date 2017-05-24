2f6554889d96d6ea83f29c08dd7ee81d174552d0
Eidelman Hernandez <eidel_hs@hotmail.com>
2017-05-24 10:10:44 -0500

    rev

--- app/Logica/Helpers/NumberToLetter.php
+++ app/Logica/Helpers/NumberToLetter.php
@@ -66,129 +66,135 @@ class NumberToLetter
         $matmil[15] = 'de billones de trillones';
         $matmil[16] = 'millones de billones de trillones';
 
-        //Zi hack
-        $float= explode('.', $num);
-        $num=$float[0];
-        $num = trim((string)@$num);
-        if ($num[0] == '-') {
-            $neg = 'menos ';
-            $num = substr($num, 1);
-        }else
-            $neg = '';
-        while ($num[0] == '0') $num = substr($num, 1);
-        if ($num[0] < '1' or $num[0] > 9) $num = '0' . $num;
-        $zeros = true;
-        $punt = false;
-        $ent = '';
-        $fra = '';
-        for ($c = 0; $c < strlen($num); $c++) {
-            $n = $num[$c];
-            if (! (strpos(".,'''", $n) === false)) {
-                if ($punt) break;
-                else{
-                    $punt = true;
-                    continue;
+
+        try {
+
+            //Zi hack
+            $float = explode('.', $num);
+            $num = $float[0];
+            $num = trim((string)@$num);
+            if ($num[0] == '-') {
+                $neg = 'menos ';
+                $num = substr($num, 1);
+            } else
+                $neg = '';
+            while ($num[0] == '0') $num = substr($num, 1);
+            if ($num[0] < '1' or $num[0] > 9) $num = '0' . $num;
+            $zeros = true;
+            $punt = false;
+            $ent = '';
+            $fra = '';
+            for ($c = 0; $c < strlen($num); $c++) {
+                $n = $num[$c];
+                if (!(strpos(".,'''", $n) === false)) {
+                    if ($punt) break;
+                    else {
+                        $punt = true;
+                        continue;
+                    }
+                } elseif (!(strpos('0123456789', $n) === false)) {
+                    if ($punt) {
+                        if ($n != '0') $zeros = false;
+                        $fra .= $n;
+                    } else
+                        $ent .= $n;
+                } else
+                    break;
+            }
+            $ent = '     ' . $ent;
+            if ($dec and $fra and !$zeros) {
+                $fin = ' coma';
+                for ($n = 0; $n < strlen($fra); $n++) {
+                    if (($s = $fra[$n]) == '0')
+                        $fin .= ' cero';
+                    elseif ($s == '1')
+                        $fin .= $fem ? ' una' : ' un';
+                    else
+                        $fin .= ' ' . $matuni[$s];
                 }
-            }elseif (! (strpos('0123456789', $n) === false)) {
-                if ($punt) {
-                    if ($n != '0') $zeros = false;
-                    $fra .= $n;
-                }else
-                    $ent .= $n;
-            }else
-                break;
-        }
-        $ent = '     ' . $ent;
-        if ($dec and $fra and ! $zeros) {
-            $fin = ' coma';
-            for ($n = 0; $n < strlen($fra); $n++) {
-                if (($s = $fra[$n]) == '0')
-                    $fin .= ' cero';
-                elseif ($s == '1')
-                    $fin .= $fem ? ' una' : ' un';
-                else
-                    $fin .= ' ' . $matuni[$s];
-            }
-        }else
-            $fin = '';
-        if ((int)$ent === 0) return 'Cero ' . $fin;
-        $tex = '';
-        $sub = 0;
-        $mils = 0;
-        $neutro = false;
-        while ( ($num = substr($ent, -3)) != '   ') {
-            $ent = substr($ent, 0, -3);
-            if (++$sub < 3 and $fem) {
-                $matuni[1] = 'una';
-                $subcent = 'as';
-            }else{
-                $matuni[1] = $neutro ? 'un' : 'uno';
-                $subcent = 'os';
-            }
-            $t = '';
-            $n2 = substr($num, 1);
-            if ($n2 == '00') {
-            }elseif ($n2 < 21)
-                $t = ' ' . $matuni[(int)$n2];
-            elseif ($n2 < 30) {
-                $n3 = $num[2];
-                if ($n3 != 0) $t = 'i' . $matuni[$n3];
-                $n2 = $num[1];
-                $t = ' ' . $matdec[$n2] . $t;
-            }else{
-                $n3 = $num[2];
-                if ($n3 != 0) $t = ' y ' . $matuni[$n3];
-                $n2 = $num[1];
-                $t = ' ' . $matdec[$n2] . $t;
-            }
-            $n = $num[0];
-            if ($n == 1) {
-                $t = ' ciento' . $t;
-            }elseif ($n == 5){
-                $t = ' ' . $matunisub[$n] . 'ient' . $subcent . $t;
-            }elseif ($n != 0){
-                $t = ' ' . $matunisub[$n] . 'cient' . $subcent . $t;
-            }
-            if ($sub == 1) {
-            }elseif (! isset($matsub[$sub])) {
-                if ($num == 1) {
-                    $t = ' mil';
-                }elseif ($num > 1){
-                    $t .= ' mil';
+            } else
+                $fin = '';
+            if ((int)$ent === 0) return 'Cero ' . $fin;
+            $tex = '';
+            $sub = 0;
+            $mils = 0;
+            $neutro = false;
+            while (($num = substr($ent, -3)) != '   ') {
+                $ent = substr($ent, 0, -3);
+                if (++$sub < 3 and $fem) {
+                    $matuni[1] = 'una';
+                    $subcent = 'as';
+                } else {
+                    $matuni[1] = $neutro ? 'un' : 'uno';
+                    $subcent = 'os';
                 }
-            }elseif ($num == 1) {
-                $t .= ' ' . $matsub[$sub] . '?n';
-            }elseif ($num > 1){
-                $t .= ' ' . $matsub[$sub] . 'ones';
-            }
-            if ($num == '000') $mils ++;
-            elseif ($mils != 0) {
-                if (isset($matmil[$sub])) $t .= ' ' . $matmil[$sub];
-                $mils = 0;
-            }
-            $neutro = true;
-            $tex = $t . $tex;
-        }
-        $tex = $neg . substr($tex, 1) . $fin;
-        //Zi hack --> return ucfirst($tex);
-        try{
-            //esto colocó Eidelman
-            if (count($float)==2){
-                if( strlen($float[1]) == 1){
-                    $float[1] = $float[1].'0';
+                $t = '';
+                $n2 = substr($num, 1);
+                if ($n2 == '00') {
+                } elseif ($n2 < 21)
+                    $t = ' ' . $matuni[(int)$n2];
+                elseif ($n2 < 30) {
+                    $n3 = $num[2];
+                    if ($n3 != 0) $t = 'i' . $matuni[$n3];
+                    $n2 = $num[1];
+                    $t = ' ' . $matdec[$n2] . $t;
+                } else {
+                    $n3 = $num[2];
+                    if ($n3 != 0) $t = ' y ' . $matuni[$n3];
+                    $n2 = $num[1];
+                    $t = ' ' . $matdec[$n2] . $t;
                 }
+                $n = $num[0];
+                if ($n == 1) {
+                    $t = ' ciento' . $t;
+                } elseif ($n == 5) {
+                    $t = ' ' . $matunisub[$n] . 'ient' . $subcent . $t;
+                } elseif ($n != 0) {
+                    $t = ' ' . $matunisub[$n] . 'cient' . $subcent . $t;
+                }
+                if ($sub == 1) {
+                } elseif (!isset($matsub[$sub])) {
+                    if ($num == 1) {
+                        $t = ' mil';
+                    } elseif ($num > 1) {
+                        $t .= ' mil';
+                    }
+                } elseif ($num == 1) {
+                    $t .= ' ' . $matsub[$sub] . '?n';
+                } elseif ($num > 1) {
+                    $t .= ' ' . $matsub[$sub] . 'ones';
+                }
+                if ($num == '000') $mils++;
+                elseif ($mils != 0) {
+                    if (isset($matmil[$sub])) $t .= ' ' . $matmil[$sub];
+                    $mils = 0;
+                }
+                $neutro = true;
+                $tex = $t . $tex;
             }
-            $ff = $float[1] == '' ? '00':$float[1] ;
+            $tex = $neg . substr($tex, 1) . $fin;
+            //Zi hack --> return ucfirst($tex);
+            try {
+                //esto colocó Eidelman
+                if (count($float) == 2) {
+                    if (strlen($float[1]) == 1) {
+                        $float[1] = $float[1] . '0';
+                    }
+                }
+                $ff = $float[1] == '' ? '00' : $float[1];
 
 
-        }catch ( \Exception $e){
-            $ff= '00';
+            } catch (\Exception $e) {
+                $ff = '00';
 
+            }
+            /**
+             * esto modifico Evhanz
+             * */
+            $end_num = ucfirst($tex) . ' con ' . $ff . '/100 Soles';
+            return $end_num;
+        }catch (\Exception $e){
+            return "Ocurrio un error :".$num.'-';
         }
-        /**
-         * esto modifico Evhanz
-         * */
-        $end_num=ucfirst($tex).' con '.$ff.'/100 Soles';
-        return $end_num;
     }
 }
