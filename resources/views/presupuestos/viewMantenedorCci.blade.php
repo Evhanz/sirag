b8b4888943cbfecff82b262e4605d94728f46897
fzelada <frank_10_501@hotmail.com>
2017-06-30 10:26:35 -0500

    CCI
    CCI

--- resources/views/presupuestos/viewMantenedorCci.blade.php
+++ resources/views/presupuestos/viewMantenedorCci.blade.php
@@ -18,6 +18,67 @@
 			<div class="box-header">
 			</div>
 			<div class="box-body">
+				<h4><strong>FACTORES GENERALES</strong></h4>
+				<hr>
+				<div class="row">
+					<div class="col-lg-4">
+						<label for="cci">Centro Costo Interno</label>
+						<input  class="form-control" name="cci" type="text" required="">
+					</div>
+					<div class="col-lg-4">
+					</div>
+					<div class="col-lg-4">
+						<label for="vigente">Vigente</label>
+						<input  class="form-control" name="vigente" type="text" required="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-3">
+						<label for="especie">Especie</label>
+						<input  class="form-control" name="especie" type="text" required="">
+					</div>
+					<div class="col-lg-3">
+						<label for="variedad">Variedad</label>
+						<input  class="form-control" name="variedad" type="text" required="">
+					</div>
+					<div class="col-lg-3">
+						<label for="fundo">Fundo</label>
+						<input  class="form-control" name="fundo" type="text" required="">
+					</div>
+					<div class="col-lg-3">
+						<label for="parron">Parron</label>
+						<input  class="form-control" name="parron" type="text" required="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-4">
+						<label for="ccosto">Centro de Costo General</label>
+						<input  class="form-control" name="ccosto" type="text" required="">
+					</div>
+					<div class="col-lg-4">
+						<label for="localidad">Localidad</label>
+						<input  class="form-control" name="localidad" type="text" required="">
+					</div>
+					<div class="col-lg-4">
+						<label for="tipoGasto">Tipo de Gasto</label>
+						<input  class="form-control" name="tipoGasto" type="text" required="">
+					</div>
+				</div>
+				<hr>
+				<h4><strong>FACTORES PRODUCCION</strong></h4>
+				<hr>
+				<div class="row">
+					<div class="col-lg-4">
+						<label for="Cliente">Cliente</label>
+						<input  class="form-control" name="Cliente" type="text" required="">
+					</div>
+					<div class="col-lg-4">
+					</div>
+					<div class="col-lg-4">
+						<label for="campana">Campaña</label>
+						<input  class="form-control" name="campana" type="text" required="">
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
